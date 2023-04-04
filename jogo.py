8cf87dd61f1e2d0b66b783a73037f95175d3d084
ApenasUmSonhador <123214565+ApenasUmSonhador@users.noreply.github.com>
2023-04-04 20:25:45 -0300

    Update jogo.py
    Apenas algumas boas praticas.

--- jogo.py
+++ jogo.py
@@ -1,20 +1,26 @@
-import os
+from os import name, system
+
 forca = True
+
 def limpar():
-    if os.name == "nt":
-        os.system('cls')
+    if name == "nt":
+        system('cls')
     else:
-        os.system('clear')
+        system('clear')
+        
 while forca == True:
     vitoria = False
     tentativas = 0
     letras_acertadas = ''
+    
     palavra_secreta = input("Bem vindo ao jogo da forca, escolha (escondido), a palavra para seus amigos acertarem!")
     limpar()
+    
     while vitoria == False:
         letra_chutada = input("Digite uma letra: ")
         tentativas+=1
         palavra_formada = ''
+        
         if len(letra_chutada)>1 or len(letra_chutada) == 0 or not letra_chutada.isalpha():
             print("Chute UMA LETRA apenas.")
             continue
@@ -22,18 +28,25 @@ while forca == True:
             if letra_chutada in palavra_secreta:
                 letras_acertadas+=letra_chutada
                 for letra_secreta in palavra_secreta:
+                    
                     if letra_secreta in letras_acertadas:
                         palavra_formada += letra_secreta
+                        
                     else:
                         palavra_formada += '*'
+                        
                 print("Palavra formada: ", palavra_formada)
+                
             else:
                 print("Voce errou, tente de novo")
             print("Tentativas: ", tentativas)
+            
             if palavra_formada  == palavra_secreta:
                 limpar()
+                vitoria = True
+                
                 print("Parabéns voce ganhou!!")
                 print("A palavra era: ", palavra_secreta)
                 print("Voce conseguiu em: ",tentativas)
-                vitoria = True
+                
                 forca = input("Deseja jogar novamente? [s]im [n]ao").strip().lower().startswith('s')
