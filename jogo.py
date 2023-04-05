4b0820bc93d48eaa468de1e7d0eae9737881450e
ApenasUmSonhador <123214565+ApenasUmSonhador@users.noreply.github.com>
2023-04-05 13:55:27 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,6 +1,7 @@
 from os import name, system
 
 forca = True
+palavra_formada = ''
 
 def limpar():
     if name == "nt":
@@ -11,34 +12,50 @@ def limpar():
 while forca == True:
     vitoria = False
     tentativas = 0
-    letras_acertadas = ''
-    
-    palavra_secreta = input("Bem vindo ao jogo da forca, escolha (escondido), a palavra para seus amigos acertarem!")
+    letras_acertadas = []
+    letras_tentadas = []
+    palavra_secreta = input("Bem vindo ao jogo da forca, escolha (escondido), a palavra para seus amigos acertarem!\n").strip().lower()
+    while len(palavra_secreta) <1 or not palavra_secreta.isalpha():
+        input("Digite uma palavra válida (formada apenas por letras): \n")
     limpar()
     
     while vitoria == False:
-        letra_chutada = input("Digite uma letra: ")
+        if tentativas >0:
+            print("Letras já tentadas: ",letras_tentadas)
+
+        letra_chutada = input("Digite uma letra: \n").strip().lower()
         tentativas+=1
+        ultima_formada = palavra_formada
         palavra_formada = ''
-        
-        if len(letra_chutada)>1 or len(letra_chutada) == 0 or not letra_chutada.isalpha():
-            print("Chute UMA LETRA apenas.")
+
+        if len(letra_chutada)>1 or not letra_chutada.isalpha():
+            print("\nChute UMA LETRA apenas.")
+            print("Palavra ainda é: ", ultima_formada)
+            tentativas -=1
             continue
+
+        elif letra_chutada in letras_tentadas:
+            print("\nVocê já chutou essa letra antes, tente outra.")
+            print("Palavra ainda é: ", ultima_formada)
+            tentativas -=1
+
         else:
+            letras_tentadas.append(letra_chutada)
             if letra_chutada in palavra_secreta:
-                letras_acertadas+=letra_chutada
+                letras_acertadas.append(letra_chutada)
                 for letra_secreta in palavra_secreta:
                     
                     if letra_secreta in letras_acertadas:
                         palavra_formada += letra_secreta
                         
                     else:
-                        palavra_formada += '*'
-                        
-                print("Palavra formada: ", palavra_formada)
+                        palavra_formada += ' *'
                 
             else:
-                print("Voce errou, tente de novo")
+                print("\nVoce errou, tente de novo")
+                print("Palavra ainda é: ", ultima_formada)
+
+            print("Palavra formada: ", palavra_formada)
             print("Tentativas: ", tentativas)
             
             if palavra_formada  == palavra_secreta:
@@ -49,4 +66,4 @@ while forca == True:
                 print("A palavra era: ", palavra_secreta)
                 print("Voce conseguiu em: ",tentativas)
                 
-                forca = input("Deseja jogar novamente? [s]im [n]ao").strip().lower().startswith('s')
+                forca = input("Deseja jogar novamente? [s]im [n]ao.\n").strip().lower().startswith('s')
